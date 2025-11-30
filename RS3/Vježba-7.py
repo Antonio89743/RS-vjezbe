15eead292f79e8104d2f185a4edba3a564445dee
Antonio89743 <99281577+Antonio89743@users.noreply.github.com>
2025-11-30 20:13:26 +0100

    #

--- RS3/Vježba-7.py
+++ RS3/Vježba-7.py
@@ -20,7 +20,7 @@ asyncio.run(main())
 
 
 
-# timer funkcija kreće s izvršavanjem po redosljedu yadanom u listi timers
+# timer funkcija kreće s izvršavanjem po redosljedu zadanom u listi timers
 # izvršava timer 1, timer 2 i timer 3 paralelno, odizimajući 1 sekundu od preostalih sekundi tog timer delay integera
 # kako je timer 1 najkraći, on prvi završava, a ostali timeri i dalje nastavljaju s odbrojavanjem, izmjenično ispisuju preostale sekunde za timer 2 i 3
 # sljedeći završen timer je timer 2
@@ -47,3 +47,13 @@ asyncio.run(main())
 # Timer 3: 2 sekundi preostalo...
 # Timer 3: 1 sekundi preostalo...
 # Timer 3: Vrijeme je isteklo!
+
+
+
+# asyncio.run(main()) pokreće main() funkciju event loop-a
+# timers lista koristi asyncio.create_task() kako bi se kreirale i pokrenule tri async timer zadaci
+# asyncio.gather(*timers) čeka da svi zadaci u timers listi budu dovršeni prije nego što nastavi dalje
+# await asyncio.gather(*timers) završava main() funkciju jednom kad su svi zadaci dovršeni
+
+# timer funkcije će se obavljati konkurentno, pa će prvi završiti timer 1, zatim timer 2, a na kraju timer 3
+# za vrijeme izvođenja timer funkcija, funkcije će predati kontrolu event loop-u koristći sleep(), event loop čeka da se timer funckije izvrše
